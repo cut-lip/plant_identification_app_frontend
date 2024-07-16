54afda62f682764453af8c490cd33da71a8b72ac
Nicholas Cutlip <cutlipn@gmail.com>
2024-07-15 18:17:52 -0700

    Add useState & useEffect to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  
+  useEffect(() => {
+   //fetch('http://localhost:8000/api/1/')
+   // .then(response)
+
+   fetchData();
+  }, {});
+
   return (
     <div className="App">
       <header className="App-header">
